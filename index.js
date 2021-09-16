cb62974d0c1c0137ebe029e6b3f3ddd8b327bb05
Viet Nguyen <0x06065a@gmail.com>
2021-09-16 16:51:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express()
 server.use(express.json())
 
 server.get('/', (_, res) => {
-  res.json({ status: 'ok', data: 'else' })
+  res.json({ status: 'ok', data: 'something useful' })
 })
 
 module.exports = server
